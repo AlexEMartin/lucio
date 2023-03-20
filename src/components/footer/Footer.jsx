ddc9719fd8dee070df4f62c7765fb7954b12d13a
alexmartin <alexeusebiomartin@gmail.com>
2023-03-20 19:48:49 -0300

    Footer typography

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,16 +6,28 @@ import youtube from "../../img/youtube.jpg";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="media-box">
-        <img className="social" src={facebook} alt="" />
-        <img className="social" src={instagram} alt="" />
-        <img className="social" src={youtube} alt="" />
+    <div>
+      <div className="footer">
+        <div className="footer-data">
+          <div className="block">
+            <h2 className="title">Social Media</h2>
+            <div className="media-box">
+              <img className="logo" src={facebook} alt="" />
+              <img className="logo" src={instagram} alt="" />
+              <img className="logo" src={youtube} alt="" />
+            </div>
+          </div>
+          <div className="block">
+            <h2 className="title">Contacto</h2>
+            <p className="phone">
+              <span style={{ color: "#888888" }}>+34 638 496 387</span>
+              <span style={{ color: "#c5c5c5" }}>
+                luciobag@gmail.com
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
-      <p className="phone">
-        <span style={{ color: "#888888", marginRight: '30px' }}>+34 638 496 387</span>
-        <span style={{ color: "#c5c5c5" }}>luciobag@gmail.com</span>{" "}
-      </p>
     </div>
   );
 };
