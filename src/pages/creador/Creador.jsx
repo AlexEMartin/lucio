ca8105065370bf2081b7ab2e66e6a8f2edd0fcfc
alexmartin <alexeusebiomartin@gmail.com>
2023-03-31 15:10:34 -0300

    Creator iframe with noland img

--- src/pages/creador/Creador.jsx
+++ src/pages/creador/Creador.jsx
@@ -2,12 +2,19 @@ import Navbar from "../../components/navbar/Navbar";
 import WhappBtn from "../../utils/whatsapp/WhappBtn";
 import Iframe from "../../components/iframes/Iframe";
 import Footer from "../../components/footer/Footer";
+import noland from "../../img/noland.jpeg";
+import "./Creador.css";
 
 const Creador = () => {
   return (
     <>
       <Navbar />
-      <Iframe url="https://www.youtube.com/watch?v=Gy-dify_Fqo" />
+      <div style={{ width: "100%", backgroundColor: "black" }}>
+        <Iframe url="https://www.youtube.com/watch?v=Gy-dify_Fqo" />
+      </div>
+      <div className="noland-container">
+        <img className="creator-noland" src={noland} alt="" />
+      </div>
       <WhappBtn />
       <Footer />
     </>
